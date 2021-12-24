a3c71961a484ac284980c98c98dc91834c20049b
Ringaile Valiaugaite <ringaile@gmail.com>
2021-12-24 10:40:09 +0100

    added votes

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,68 @@ const App = () => {
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
 
+  // Holds the amount of token each member has in state.
+const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+// The array holding all of our members addresses.
+const [memberAddresses, setMemberAddresses] = useState([]);
+
+// A fancy function to shorten someones wallet address, no need to show the whole thing. 
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
+// This useEffect grabs all our the addresses of our members holding our NFT.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+  
+  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+  // with tokenId 0.
+  bundleDropModule
+    .getAllClaimerAddresses("0")
+    .then((addresess) => {
+      console.log("🚀 Members addresses", addresess)
+      setMemberAddresses(addresess);
+    })
+    .catch((err) => {
+      console.error("failed to get member list", err);
+    });
+}, [hasClaimedNFT]);
+
+// This useEffect grabs the # of token each member holds.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  // Grab all the balances.
+  tokenModule
+    .getAllHolderBalances()
+    .then((amounts) => {
+      console.log("👜 Amounts", amounts)
+      setMemberTokenAmounts(amounts);
+    })
+    .catch((err) => {
+      console.error("failed to get token amounts", err);
+    });
+}, [hasClaimedNFT]);
+
+// Now, we combine the memberAddresses and memberTokenAmounts into a single array
+const memberList = useMemo(() => {
+  return memberAddresses.map((address) => {
+    return {
+      address,
+      tokenAmount: ethers.utils.formatUnits(
+        // If the address isn't in memberTokenAmounts, it means they don't
+        // hold any of our token.
+        memberTokenAmounts[address] || 0,
+        18,
+      ),
+    };
+  });
+}, [memberAddresses, memberTokenAmounts]);
+
   // Another useEffect!
   useEffect(() => {
     // We pass the signer to the sdk, which enables us to interact with
@@ -65,12 +127,36 @@ const App = () => {
     );
   }
 
-  // Add this little piece!
+  // If the user has already claimed their NFT we want to display the interal DAO page to them
+  // only DAO members will see this. Render all the members + token amounts.
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
         <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   };
